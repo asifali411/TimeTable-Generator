23c935b34ba3daf0140ee3ebc9e19ea32f682dca
Galon123 <leejohnsanthosh12345@gmail.com>
2025-12-08 15:54:56 +0530

    BoilerPlate code added

--- backend/Generations/School_timetable.py
+++ backend/Generations/School_timetable.py
@@ -1,1 +1,14 @@
-1
+from ortools.sat.python import cp_model
+
+model = cp_model.CpModel()
+
+#Maybe can add Saturday too
+days=['Mon','Tue','Wed','Thu','Fri']
+
+#Can be inputted in later
+rooms=[]
+times=[]
+teachers=[]
+
+#Feel free to change the structure, but please explain, if you do
+shifts={}
